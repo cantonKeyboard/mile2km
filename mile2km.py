8a5bfcf090b3dbc34dab491360a8e87131fdc399
Numplex <132838206+cantonKeyboard@users.noreply.github.com>
2024-06-01 22:54:01 +0530

    Adding Script (Commit #1)
    Adding the main script

---
+++ mile2km.py
@@ -0,0 +1,13 @@
+def mile2km():
+    mile = 1.60934
+    distance =  float(input('Miles: '))
+
+    print(distance * float(mile))
+    print(distance)
+
+def km2mile():
+    km = 0.621371
+    distance1 = float(input('Kilometers: '))
+
+    print(distance1 * float(km))
+    print(km)
